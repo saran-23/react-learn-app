4b35d86d7214715cfe2754f9220bfb17679ca7cf
saran-23 <romansaran446@gmail.com>
2021-01-02 08:13:28 +0530

    using multiple props

--- jsx/src/CommentDetail.js
+++ jsx/src/CommentDetail.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import faker from 'faker';
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+
     return (
         <div className="comment">
         <a href="/" className="avatar">
             <img alt="avatar"  src='https://source.unsplash.com/random'/>
         </a>
         <div className="content">
-            <a href="/" className="author">Saran </a>
+            <a href="/" className="author">{props.author}</a>
         </div>
         <div className="metadata">
             <span className="date">
-                Today at 6:00pm
+                {props.timeAgo}
             </span>
         </div>
         <div className="text">Nice Blog post!</div>
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,13 +6,9 @@ const App = () =>{
 
             <div className="ui container comments">
             
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
+                <CommentDetail author='saran' timeAgo="Today at 4:45AM" />
+                <CommentDetail author='sam' timeAgo="Today at 7:25AM"/>
+                <CommentDetail author='alex' timeAgo="Yesterday at 1:45AM"/>
             </div>
             
             

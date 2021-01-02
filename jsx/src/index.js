3d503832259dabd4cad88f0fce0ace3e46f27e79
saran-23 <romansaran446@gmail.com>
2021-01-02 08:56:25 +0530

    creating ApprovalCard Component and used semanitc card

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 const App = () =>{
     return(
 
+
             <div className="ui container comments">
-            
+                <ApprovalCard />
                 <CommentDetail author='saran' timeAgo="Today at 4:45AM" imageSrc='https://source.unsplash.com/random' Desc=" the first comment" />
                 <CommentDetail author='sam' timeAgo="Today at 7:25AM" imageSrc='https://source.unsplash.com/random' Desc="I am  the Second comment" />
                 <CommentDetail author='alex' timeAgo="Yesterday at 1:45AM" imageSrc='https://source.unsplash.com/random' Desc="i am the last comment" />
